892fe6861b7c74c8505e5ba5e33caf63c00e084d
gersin231 <gersonsouza4839@gmail.com>
2025-04-07 12:00:17 -0400

    Update main.js

--- ajax/main.js
+++ ajax/main.js
@@ -1,6 +1,8 @@
 import modalCadastro from "./modalCadastro.js"
 
-
+document.addEventListener('DOMContentLoaded',()=>{
+    let main = document.querySelector('#infoCadastro')
+})
 console.log(modalCadastro());
 
 
